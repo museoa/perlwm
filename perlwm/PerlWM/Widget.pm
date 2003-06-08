3735ae25577dea16a9edf93061bdc0691bb495bc
rdw <>
2003-06-08 13:41:39 +0000

    Tweaked default font charset

--- perlwm/PerlWM/Widget.pm
+++ perlwm/PerlWM/Widget.pm
@@ -16,11 +16,9 @@ use base qw(PerlWM::X::Window);
 sub init {
   my($class, $x) = @_;
 
-#  $x->color_add('widget_background', 'white');
-#  $x->color_add('widget_foreground', 'black');
   $x->color_add('widget_background', 'black');
   $x->color_add('widget_foreground', 'white');
-  $x->font_add('widget_font', '-b&h-lucida-medium-r-normal-*-*-100-*-*-p-*-iso8859-15');
+  $x->font_add('widget_font', '-b&h-lucida-medium-r-normal-*-*-100-*-*-p-*-iso8859-1');
   $x->gc_add('widget', { foreground => 'widget_foreground',
 			 background => 'widget_background',
 			 font => 'widget_font' });
