b5fbc1724995004722e2364d0aa1a8e7e818a008
rdw <>
2003-12-19 11:59:15 +0000

    Temporary fix for old mouse move/resize action

--- perlwm/PerlWM/Action/Builtin.pm
+++ perlwm/PerlWM/Action/Builtin.pm
@@ -30,11 +30,11 @@ sub move_opaque {
     if ($frame) {
       $frame->ConfigureWindow(stack_mode => 'Above');
     }
-    $state->{orig_position} = $client->position();
+    $state->{orig_position} = $frame->position();
   }
   if ($event->{delta}->[0] && $event->{delta}->[1]) {
-    $client->configure(position => [$state->{orig_position}->[0] + $event->{delta}->[0],
-				    $state->{orig_position}->[1] + $event->{delta}->[1]])
+    $frame->configure(position => [$state->{orig_position}->[0] + $event->{delta}->[0],
+				   $state->{orig_position}->[1] + $event->{delta}->[1]])
   }
   return 1;
 }
@@ -58,8 +58,8 @@ sub resize_opaque {
     if ($frame) {
       $frame->ConfigureWindow(stack_mode => 'Above');
     }
-    $state->{orig_position} = $client->position();
-    $state->{orig_size} = $client->size();
+    $state->{orig_position} = $frame->position();
+    $state->{orig_size} = $frame->size();
     my $click = [$event->{press}->{root_x}, $event->{press}->{root_y}];
     my $middle = [$state->{orig_position}->[0] + ($state->{orig_size}->[0] / 2),
 		  $state->{orig_position}->[1] + ($state->{orig_size}->[1] / 2)];
@@ -78,7 +78,7 @@ sub resize_opaque {
 	$size->[$_] += $event->{delta}->[$_];
       }
     }
-    $client->configure(position => $position, size => $size);
+    $frame->configure(position => $position, size => $size);
   }
   return 1;
 }
